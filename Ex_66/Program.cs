455d77409a19439909dfedef1a1cbfa669c2a97c
Darya <darya.ogurtsova.99@mail.ru>
2022-09-10 19:13:17 +0300

    Добавлено задание 66

---
+++ Ex_66/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число:");
+string? aString = Console.ReadLine();
+int a = int.Parse(aString);
+Console.WriteLine("Введите второе число:");
+string? bString = Console.ReadLine();
+int b = int.Parse(bString);
+int i = a;
+int sum = 0;
+while (i <= b){
+    sum = sum + i;
+    i ++;
+}
+Console.WriteLine(sum);
