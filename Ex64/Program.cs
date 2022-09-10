a1c9c5fc225a2e6c220eefa1ffea16bd38f97e16
Darya <darya.ogurtsova.99@mail.ru>
2022-09-10 19:09:04 +0300

    Добавлено задание 64

---
+++ Ex64/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число:");
+string? aString = Console.ReadLine();
+int a = int.Parse(aString);
+Console.WriteLine("Введите второе число:");
+string? bString = Console.ReadLine();
+int b = int.Parse(bString);
+int i = a;
+while (i < b){
+    Console.WriteLine(i);
+    i ++;
+}
