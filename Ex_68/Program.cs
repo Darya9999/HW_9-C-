49a48a70eaabb41e4aef5b2db55941e24c359ad2
Darya <darya.ogurtsova.99@mail.ru>
2022-09-16 00:17:28 +0300

    добавлено задание 68

---
+++ Ex_68/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите первое число:");
+string? nString = Console.ReadLine();
+int n = int.Parse(nString);
+Console.WriteLine("Введите второе число:");
+string? mString = Console.ReadLine();
+int m = int.Parse(mString);
+
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return A(n - 1, 1);
+    else
+      return A(n - 1, A(n, m - 1));
+
